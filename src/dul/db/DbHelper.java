9b8afd1f2195c02f9341f7a45ec006d99ce0c8ae
koyuzurevoer <48278683+koyuzurevoer@users.noreply.github.com>
2023-06-15 08:56:53 +0700

    Anggota connect with db

---
+++ src/dul/db/DbHelper.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dul.db;
+
+import dul.model.Anggota;
+import dul.dao.AnggotaDaoImpl;
+import dul.dao.AnggotaDao;
+import com.mysql.cj.jdbc.*;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class DbHelper {
+    private static Connection connection;
+    
+    public static Connection getConnection() throws SQLException {
+        if(connection==null){
+            MysqlDataSource dataSource = new  MysqlDataSource();
+            dataSource.setURL("jdbc:mysql://localhost/pbo_2211083017");
+            dataSource.setUser("root");
+            dataSource.setPassword("");
+            connection = dataSource.getConnection();
+        }
+        return connection;
+    }
+    
+    public static void main(String[] args){
+        try {
+            connection = DbHelper.getConnection();
+            AnggotaDao dao = new AnggotaDaoImpl(connection);
+            Anggota anggota = new Anggota("A001", "Ali", "Padang", "L");
+            dao.insert(anggota);
+            JOptionPane.showMessageDialog(null, "Entri data Ok");
+        } catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
+}
